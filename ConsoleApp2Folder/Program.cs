24878bf0352083800899669c19dfca5d90aabf62
Hamza Nawaz <ac112979@avcol.school.nz>
2020-07-01 09:32:19 +1200

    Add project files.

---
+++ ConsoleApp2Folder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2Folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Age = 10;
+            string Name = "Hamza";
+            Console.WriteLine("Hello {0} and your age is {1}", Name, Age);
+
+        }
+    }
+}
